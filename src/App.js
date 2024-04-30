9615aecbf4ffb93e5fc8321b2b485f989ae1266f
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-29 22:19:48 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
+import { useState } from 'react';
 import './styles.css';
 
 function App() {
+
+  const [answer, setAnswer] = useState('');
+  const [message, setMessage] = useState({text: '', color:"black"});
+
+  const draggables = ["HTML" , "JavaScript" , "PHP", "C++", "React.js"];
+
+  const checkAnswer = () => {
+
+    const correctAnswer = "html";
+
+      if(answer.toLowerCase() === correctAnswer){
+
+        setMessage({text:"Your Answer Is Correct", color: "green"});
+
+      } else {
+
+        setMessage({text: "Try Again", color: "Red"})
+
+      }
+
+  };
+
+  const ondragstart = (e, value) => {
+
+    e.dataTransfer.setData('text/plain', value);
+
+  };
+
+  ondragover = (e) => {
+
+    e.preventDefault();
+
+  }
+
+  const onDrop = (e) => {
+
+    e.preventDefault();
+    const answer = e.dataTransfer.getData('text/plain');
+    setAnswer(answer);
+
+  }
+
   return (
     <>
 
       <h1>Welcome</h1>
       <div className="container">
         <div id="answerPool">
-          <p>PHP</p>
-          <p>C++</p>
-          <p>HTML</p>
-          <p>React</p>
-          <p>JavaScript</p>
+          {draggables.map((item, index) => (
+          <p 
+            className='draggable'
+            draggable = "true"
+            ondragstart={(event) => ondragstart(event, item)}
+          >{item}</p>
+          ))}
         </div>
       <label htmlFor="blank">
         <input id="blank" type="text" />
