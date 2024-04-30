2d9436d6340d6be930cfc6d525971982eac0a416
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-29 23:12:02 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -2,74 +2,76 @@ import { useState } from 'react';
 import './styles.css';
 
 function App() {
-
   const [answer, setAnswer] = useState('');
-  const [message, setMessage] = useState({text: '', color:"black"});
-
-  const draggables = ["HTML" , "JavaScript" , "PHP", "C++", "React.js"];
+  const [message, setMessage] = useState({ text: '', color: 'black' });
+  const draggables = ['HTML', 'JavaScript', 'PHP', 'C++', 'React.js'];
 
   const checkAnswer = () => {
-
-    const correctAnswer = "html";
-
-      if(answer.toLowerCase() === correctAnswer){
-
-        setMessage({text:"Your Answer Is Correct", color: "green"});
-
-      } else {
-
-        setMessage({text: "Try Again", color: "Red"})
-
-      }
-
+    const correctAnswer = 'html';
+    if (answer.toLowerCase() === correctAnswer) {
+      setMessage({ text: 'Your Answer Is Correct', color: 'green' });
+    } else {
+      setMessage({ text: 'Try Again', color: 'red' });
+    }
   };
 
   const onDragStart = (e, value) => {
-
     e.dataTransfer.setData('text/plain', value);
-
   };
 
   const onDragOver = (e) => {
-
     e.preventDefault();
-
-  }
+  };
 
   const onDrop = (e) => {
-
     e.preventDefault();
-    const answer = e.dataTransfer.getData('text/plain');
-    setAnswer(answer);
+    const droppedText = e.dataTransfer.getData('text/plain');
+    setAnswer(droppedText);
+  };
 
-  }
+  const handleInputChange = (e) => {
+    setAnswer(e.target.value);
+  };
 
   return (
     <>
-
       <h1>Welcome</h1>
       <div className="container">
-        <div id="answerPool" >
+        <div id="answerPool">
           {draggables.map((item, index) => (
-          <p
-            key={index}
-            className='draggable'
-            draggable = "true"
-            onDragStart={(event) => onDragStart(event, item)}
-          >{item}</p>
+            <p
+              key={index}
+              className="draggable"
+              draggable="true"
+              onDragStart={(event) => onDragStart(event, item)}
+            >
+              {item}
+            </p>
           ))}
         </div>
-        <label
-        htmlFor="blank"
-        onDragOver = {onDragOver}
-        onDrop = {onDrop}
-        value = {answer}
+        <label htmlFor="blank">
+          It Stands For Hypertext Markup Language
+          <input
+            id="blank"
+            type="text"
+            value={answer}
+            onChange={handleInputChange}
+            onDragOver={onDragOver}
+            onDrop={onDrop}
+          />
+        </label>
+        <button id="checkAnswer" onClick={checkAnswer}>
+          Check Answer
+        </button>
+        <div
+          id="answerMessage"
+          style={{
+            display: message.text ? 'block' : 'none',
+            color: message.color,
+          }}
         >
-        <input id="blank" type="text" />
-        It Stands For Hypertext Markup Language
-      </label>
-      <button id="checkAnswer" onClick={checkAnswer}>Check Answer</button>
-      <div id="answerMessage" style={{ display: message.text ? 'block' : 'none', color: message.color }}>{message.text}</div>
+          {message.text}
+        </div>
       </div>
     </>
   );
